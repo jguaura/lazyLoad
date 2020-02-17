01c9d4a128bf7c2562979e17e8cd9a5c441f1c04
Jose Guaura <jguaura@8amarketing.com>
2020-02-17 11:49:39 -0300

    Pushing the files

---
+++ lazyLoad.js
@@ -0,0 +1,18 @@
+const targets = document.querySelectorAll('img')
+
+const lazyLoad = target => {
+    const io = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            console.log('💘')
+            if(entry.isIntersecting) {
+                const img = entry.target
+                const src = img.getAttribute('data-lazy')
+                img.setAttribute('src', src)
+                img.classList.add('fade')
+                observer.disconnect()
+            }
+        })
+    })
+    io.observe(target) 
+}
+targets.forEach(lazyLoad)
